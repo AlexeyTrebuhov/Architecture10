953d8f151b0380dc155770a282d035b37a1a5039
Alex_T <saler@inbox.ru>
2023-01-12 17:44:44 +0300

    HW Lesson10

---
+++ ClinicService/Models/Requests/CreatePetRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ClinicService.Models.Requests
+{
+    public class CreatePetRequest
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int PetId { get; internal set; }
+        public DateTime ConsultationDate { get; internal set; }
+        public string Description { get; internal set; }
+    }
+}
